df96786eeebb1949ab7bfc79d47e2f7c91489218
j <664943056@qq.com>
2019-01-04 13:50:05 +0800

    123123

--- DXApplication1/DXApplication1/Class1.cs
+++ DXApplication1/DXApplication1/Class1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DXApplication1
 {
     class Class1
     {
+
+
+        int i = 0;
     }
 }
