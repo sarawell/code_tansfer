41a66647f41aa0a33b172a06a44c158ed340c63c
sarawell <664943056@qq.com>
2019-01-04 14:07:10 +0800

    add datatime test

---
+++ DXApplication1/datetimeText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace datetimeText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = System.DateTime.Now;
+            string i=dt.
+        }
+    }
+}
